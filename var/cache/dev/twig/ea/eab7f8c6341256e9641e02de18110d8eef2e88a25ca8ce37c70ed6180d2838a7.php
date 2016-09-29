73342984f4007720e0155b170f3f96b9631afd29
root <root@fabricemv-HP.(none)>
2016-09-29 13:26:29 +0100

    Template presque fini

--- var/cache/dev/twig/ea/eab7f8c6341256e9641e02de18110d8eef2e88a25ca8ce37c70ed6180d2838a7.php
+++ var/cache/dev/twig/ea/eab7f8c6341256e9641e02de18110d8eef2e88a25ca8ce37c70ed6180d2838a7.php
@@ -15,26 +15,26 @@ class __TwigTemplate_f17da733bd7ba47fef836983e6a778666da6c4c3ec552ec026e2ee7cf00
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_dc9ee4972c4c7a4046f70dbb572946f9045e7bc77865d1fad0e0a3ed07f2e576 = $this->env->getExtension("native_profiler");
-        $__internal_dc9ee4972c4c7a4046f70dbb572946f9045e7bc77865d1fad0e0a3ed07f2e576->enter($__internal_dc9ee4972c4c7a4046f70dbb572946f9045e7bc77865d1fad0e0a3ed07f2e576_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "LGPUserBundle:Avis:voir.html.twig"));
+        $__internal_1740486dc12746eef6514ba39ac4081e7d1151bbfda1b9099594fdf6036bfe95 = $this->env->getExtension("native_profiler");
+        $__internal_1740486dc12746eef6514ba39ac4081e7d1151bbfda1b9099594fdf6036bfe95->enter($__internal_1740486dc12746eef6514ba39ac4081e7d1151bbfda1b9099594fdf6036bfe95_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "LGPUserBundle:Avis:voir.html.twig"));
 
         // line 1
-        echo "<h1>Note: ";
+        echo "<h3>Note: </h3>";
         echo twig_escape_filter($this->env, $this->getAttribute((isset($context["avis"]) ? $context["avis"] : $this->getContext($context, "avis")), "note", array()), "html", null, true);
-        echo "</h1>
-<h3>Prof: ";
+        echo "
+<h3>Prof: </h3> ";
         // line 2
         echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute((isset($context["avis"]) ? $context["avis"] : $this->getContext($context, "avis")), "prof", array()), "prenoms", array()), "html", null, true);
         echo " ";
         echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute((isset($context["avis"]) ? $context["avis"] : $this->getContext($context, "avis")), "prof", array()), "nom", array()), "html", null, true);
-        echo "</h3>
+        echo "
 <h3>Commentaire</h3>
 <p>";
         // line 4
         echo twig_escape_filter($this->env, $this->getAttribute((isset($context["avis"]) ? $context["avis"] : $this->getContext($context, "avis")), "commentaire", array()), "html", null, true);
         echo "</p>";
         
-        $__internal_dc9ee4972c4c7a4046f70dbb572946f9045e7bc77865d1fad0e0a3ed07f2e576->leave($__internal_dc9ee4972c4c7a4046f70dbb572946f9045e7bc77865d1fad0e0a3ed07f2e576_prof);
+        $__internal_1740486dc12746eef6514ba39ac4081e7d1151bbfda1b9099594fdf6036bfe95->leave($__internal_1740486dc12746eef6514ba39ac4081e7d1151bbfda1b9099594fdf6036bfe95_prof);
 
     }
 
@@ -55,8 +55,8 @@ class __TwigTemplate_f17da733bd7ba47fef836983e6a778666da6c4c3ec552ec026e2ee7cf00
 
     public function getSource()
     {
-        return "<h1>Note: {{ avis.note }}</h1>
-<h3>Prof: {{ avis.prof.prenoms }} {{ avis.prof.nom }}</h3>
+        return "<h3>Note: </h3>{{ avis.note }}
+<h3>Prof: </h3> {{ avis.prof.prenoms }} {{ avis.prof.nom }}
 <h3>Commentaire</h3>
 <p>{{ avis.commentaire }}</p>";
     }
