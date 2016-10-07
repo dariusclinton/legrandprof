9624567abdf3862070c2095aa4557599ca9fbf42
Darius Clinton Tsafack <clintontsafack@gmail.com>
2016-10-07 07:36:34 +0100

    sonataAdmin suite

--- src/LGP/UserBundle/Controller/MainController.php
+++ src/LGP/UserBundle/Controller/MainController.php
@@ -3,11 +3,30 @@
 namespace LGP\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class MainController extends Controller
 {
-    public function indexAction()
-    {
-        // return $this->render('LGPUserBundle:Main:index.html.twig');
-    }
+
+  /**
+   * @Security("has_role('ROLE_ADMIN')")
+   */
+  public function indexAction()
+  {
+    $em = $this->getDoctrine()->getManager();
+    
+    $repProf = $em->getRepository('LGPUserBundle:Prof');
+    $repParent = $em->getRepository('LGPUserBundle:Parents');
+    $repCourse = $em->getRepository('LGPCourseBundle:Cours');
+
+    $profs = $repProf->findAll();
+    $parents = $repParent->findAll();
+    $cours = $repCourse->findAll();
+
+    return $this->render('LGPUserBundle:User:index.html.twig', array(
+      'profs'   => $profs,
+      'parents' => $parents,
+      'cours'   => $cours
+    ));
+  }
 }
---
+++ src/LGP/UserBundle/Repository/ProfRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tsafack
+ * Date: 07/10/16
+ * Time: 06:21
+ */
+
+namespace LGP\UserBundle\Repository;
+
+
+class ProfRepository extends \Doctrine\ORM\EntityRepository
+{
+
+}
